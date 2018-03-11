7e835ce425005471bc95426a663b39f6513221d8
notomo <notomo.motono@gmail.com>
2018-03-11 13:39:34 +0900

    [add]file,directory/buffer_relative source

---
+++ rplugin/python3/curstr/action/source/directory/buffer_relative.py
@@ -0,0 +1,16 @@
+
+from os.path import join
+
+from curstr.action.group import ActionGroup, Directory, FileDispatcher
+from curstr.action.source.base import ActionSource as Base
+from curstr.custom import ActionSourceOption
+
+
+class ActionSource(Base):
+
+    _DISPATCHER_CLASS = FileDispatcher
+
+    def _create_action_group(self, option: ActionSourceOption) -> ActionGroup:
+        path = self._vim.call('expand', '<cfile>')
+        absolute_path = join(self._vim.call('expand', '%:p:h'), path)
+        return self._dispatcher.dispatch((Directory, absolute_path))

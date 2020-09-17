8d3b0081e22e74acf25f9132eb655f07bc938dc0
notomo <notomo.motono@gmail.com>
2020-09-18 08:33:43 +0900

    Fix file/path source

--- lua/curstr/action_source/file/path.lua
+++ lua/curstr/action_source/file/path.lua
@@ -2,7 +2,10 @@ local M = {}
 
 M.create = function(self)
   local path, position = self.cursor:file_path_with_position()
-  local abs_path = vim.fn.fnamemodify(path, ":p")
+  local abs_path = vim.fn.fnamemodify(vim.fn.expand(path), ":p")
+  if not self.filelib.readable(abs_path) then
+    return nil
+  end
   return self:to_group("file", {path = abs_path, position = position})
 end
 

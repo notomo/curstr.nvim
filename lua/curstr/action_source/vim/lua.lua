02a1da18b34e010ef3ca4f454c1db45f25d490e4
notomo <notomo.motono@gmail.com>
2022-02-04 09:03:27 +0900

    Improve vim/lua source for opt package

--- lua/curstr/action_source/vim/lua.lua
+++ lua/curstr/action_source/vim/lua.lua
@@ -4,9 +4,25 @@ function M.create(self)
   local cword = self.cursor:word("./-")
   local name = cword:gsub("%.", "/")
 
-  local paths = vim.api.nvim_get_runtime_file(("lua/%s.lua"):format(name), true)
-  vim.list_extend(paths, vim.api.nvim_get_runtime_file(("lua/%s/init.lua"):format(name), true))
+  local lua = ("lua/%s.lua"):format(name)
+  local init_lua = ("lua/%s/init.lua"):format(name)
+  local paths = vim.api.nvim_get_runtime_file(lua, true)
+  vim.list_extend(paths, vim.api.nvim_get_runtime_file(init_lua, true))
+
   for _, path in ipairs(paths) do
+    if self.filelib.readable(path) then
+      return self:to_group("file", { path = path })
+    end
+  end
+
+  local opt_paths = {}
+  local pack_path = vim.split(vim.o.packpath, ",", true)[1]
+  pack_path = vim.fn.fnamemodify(pack_path, ":p")
+  local pack_pattern = pack_path .. "pack/*/opt/*/"
+  vim.list_extend(opt_paths, vim.fn.glob(pack_pattern .. lua, false, true))
+  vim.list_extend(opt_paths, vim.fn.glob(pack_pattern .. init_lua, false, true))
+
+  for _, path in ipairs(opt_paths) do
     if self.filelib.readable(path) then
       return self:to_group("file", { path = path })
     end

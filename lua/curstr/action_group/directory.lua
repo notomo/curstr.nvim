225d196c408aa1febfb20cb09655f6d4fc3a59f8
notomo <notomo.motono@gmail.com>
2023-03-26 12:07:26 +0900

    Fix to suppress lcd message

--- lua/curstr/action_group/directory.lua
+++ lua/curstr/action_group/directory.lua
@@ -1,27 +1,29 @@
+local filelib = require("curstr.lib.file")
+
 local M = {}
 
 function M.after(_, _) end
 
 function M.action_open(self)
-  vim.cmd.lcd(self.path)
+  filelib.lcd(self.path)
   self:after(self.path)
 end
 
 function M.action_tab_open(self)
   vim.cmd.tabedit(self.path)
-  vim.cmd.lcd(self.path)
+  filelib.lcd(self.path)
   self:after(self.path)
 end
 
 function M.action_vertical_open(self)
   vim.cmd.vsplit(self.path)
-  vim.cmd.lcd(self.path)
+  filelib.lcd(self.path)
   self:after(self.path)
 end
 
 function M.action_horizontal_open(self)
   vim.cmd.split(self.path)
-  vim.cmd.lcd(self.path)
+  filelib.lcd(self.path)
   self:after(self.path)
 end
 
--- lua/curstr/lib/file.lua
+++ lua/curstr/lib/file.lua
@@ -8,4 +8,12 @@ function M.is_directory(file_path)
   return vim.fn.isdirectory(file_path) ~= 0
 end
 
+function M._escape(path)
+  return ([[`='%s'`]]):format(path:gsub("'", "''"))
+end
+
+function M.lcd(path)
+  vim.cmd.lcd({ args = { M._escape(path) }, mods = { silent = true } })
+end
+
 return M
